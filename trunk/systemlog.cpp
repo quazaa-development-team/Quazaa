6445dda529f4656b023cfc81d3afce29f2e4a4fc
brov <brov@users.sourceforge.net>
2010-05-18 18:38:51 +0000

    Fixed System Logger.

--- trunk/systemlog.cpp
+++ trunk/systemlog.cpp
@@ -1,9 +1,11 @@
 #include "systemlog.h"
+#include <QMetaType>
 
 SystemLog systemLog;
 
 SystemLog::SystemLog()
 {
+	qRegisterMetaType<LogSeverity::Severity>("LogSeverity::Severity");
 }
 
 void SystemLog::postLog(QString message, LogSeverity::Severity severity)
