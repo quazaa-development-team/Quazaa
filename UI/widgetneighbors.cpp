22be4f098a8a1baaae478d89b66efa5dfb65e890
Joseph Crowell <joseph.w.crowell@gmail.com>
2010-05-22 08:22:49 +0000

    Removed the old Gnutella Icons from the resource file. Added Content to the G2 stats toolbar in Neighbors.

--- UI/widgetneighbors.cpp
+++ UI/widgetneighbors.cpp
@@ -13,6 +13,14 @@ WidgetNeighbors::WidgetNeighbors(QWidget *parent) :
     ui(new Ui::WidgetNeighbors)
 {
     ui->setupUi(this);
+	labelG2StatsIcon = new QLabel();
+	QPixmap const pixmapG2Stats = QPixmap(":/Resource/Networks/Gnutella2.png");
+	labelG2StatsIcon->setPixmap(pixmapG2Stats);
+	labelG2StatsIcon->setScaledContents(true);
+	labelG2StatsIcon->setMaximumSize(16, 16);
+	labelG2Stats = new QLabel();
+	ui->toolBarG2->addWidget(labelG2StatsIcon);
+	ui->toolBarG2->addWidget(labelG2Stats);
 	restoreState(quazaaSettings.WinMain.NeighborsToolbars);
 	connect(&skinSettings, SIGNAL(skinChanged()), this, SLOT(skinChangeEvent()));
 	skinChangeEvent();
@@ -39,6 +47,9 @@ void WidgetNeighbors::skinChangeEvent()
 {
 	ui->toolBarControls->setStyleSheet(skinSettings.toolbars);
 	ui->toolBarNetworks->setStyleSheet(skinSettings.toolbars);
+	ui->toolBarAres->setStyleSheet(skinSettings.toolbars);
+	ui->toolBarG2->setStyleSheet(skinSettings.toolbars);
+	ui->toolBarEDonkey->setStyleSheet(skinSettings.toolbars);
 }
 
 void WidgetNeighbors::setModel(QAbstractItemModel *model)
@@ -65,7 +76,16 @@ void WidgetNeighbors::on_actionSettings_triggered()
 
 void WidgetNeighbors::updateG2()
 {
+	bool bEmit = (sender() != 0);
 
+	if( Network.m_pSection.tryLock(50) && Datagrams.m_pSection.tryLock(50) && bEmit  )
+	{
+		labelG2Stats->setText(tr(" %1 Hubs, %2 Leaves, %3 B/s In:%4 B/s Out").arg(Network.m_nHubsConnected).arg(Network.m_nLeavesConnected).arg(Network.DownloadSpeed() + Datagrams.DownloadSpeed()).arg(Network.UploadSpeed() + Datagrams.UploadSpeed()));
+		Network.m_pSection.unlock();
+		Datagrams.m_pSection.unlock();
+	} else {
+		labelG2Stats->setText(" 0 Hubs, 0 Leaves, 0 B/s In:0 B/s Out");
+	}
 }
 
 void WidgetNeighbors::updateAres()
