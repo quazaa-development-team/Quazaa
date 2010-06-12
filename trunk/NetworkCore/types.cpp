fd26e888a3dbfdd8b4669ef8b257c15e55cf4faf
brov <brov@users.sourceforge.net>
2010-06-12 18:27:53 +0000

    Fixed warning when compiling with -fstrict-aliasing

--- trunk/NetworkCore/types.cpp
+++ trunk/NetworkCore/types.cpp
@@ -7,8 +7,14 @@ uint qHash(const QUuid& key)
 	nHash ^= key.data1;
 	nHash ^= key.data2;
 	nHash ^= (key.data3 << 16);
-	nHash ^= *(quint32*)&key.data4[0];
-	nHash ^= *(quint32*)&key.data4[4];
+	/*
+	The void* cast below is correct,
+	this is to avoid warnings like:
+	"dereferencing type-punned pointer will break strict-aliasing rules"
+	when compiling with -fstrict-aliasing (enabled by default when using -O2)
+	*/
+	nHash ^= *(quint32*)(void*)&key.data4[0];
+	nHash ^= *(quint32*)(void*)&key.data4[4];
 
     return nHash;
 }
