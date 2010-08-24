0dda610c98d52b6000a23a98c8f5ecc189925637
brov <brov@users.sourceforge.net>
2010-08-24 20:13:29 +0000

    Dammit! I must blame TortoiseSVN or me :D Forgot 2 files...

---
+++ NetworkCore/Hashes/Hash.h
@@ -0,0 +1,37 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include "types.h"
+
+struct invalid_hash_exception{};
+
+class CHash
+{
+
+public:
+	enum Algorithm {SHA1, MD5, MD4};
+
+protected:
+	void*				m_pContext;
+	bool				m_bFinalized;
+	CHash::Algorithm	m_nHashAlgorithm;
+	QByteArray			m_baRawValue;
+
+public:
+	CHash(CHash::Algorithm algo);
+	CHash(QByteArray baRaw, CHash::Algorithm algo);
+
+	static int	ByteCount(CHash::Algorithm algo);
+
+	static CHash* FromURN(QString sURN);
+	static CHash* FromRaw(QByteArray& baRaw, CHash::Algorithm algo);
+
+	QString ToURN();
+	QString ToString();
+	QByteArray RawValue();
+
+	void AddData(const char* pData, quint32 nLength);
+	void AddData(QByteArray& baData);
+};
+
+#endif // HASH_H
