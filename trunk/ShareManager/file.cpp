abe927ba56dc1532de033d0b39ce90c2a4fa4ce1
old_death <old_death@users.sourceforge.net>
2011-06-03 15:55:03 +0000

    Superclass for library, search and download files. Use the implemented tags to store information about file status (search, downloading, library, share group etc.)

---
+++ trunk/ShareManager/file.cpp
@@ -0,0 +1,26 @@
+#include "file.h"
+
+CFile::CFile(QObject *parent) :
+    QFile(parent)
+{
+}
+
+bool CFile::isTagged(QString sTag)
+{
+	QSet::ConstIterator i = m_setTags.find( sTag );
+
+	if ( i != m_setTags.end() )
+		return true;
+
+	return false;
+}
+
+void CFile::addTag(QString sTag)
+{
+	m_setTags.insert( sTag );
+}
+
+bool CFile::removeTag(QString sTag)
+{
+	return m_setTags.remove( sTag );
+}
