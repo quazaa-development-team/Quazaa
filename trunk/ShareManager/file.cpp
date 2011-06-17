0be6bfebb9fcc9a2ea9cff69cb7ee1053bbcc725
old_death <old_death@users.sourceforge.net>
2011-06-17 15:53:53 +0000

    CFile: Implemented file size management.

--- trunk/ShareManager/file.cpp
+++ trunk/ShareManager/file.cpp
@@ -1,16 +1,17 @@
 #include "file.h"
 
 CFile::CFile(QObject *parent) :
-	QFile( parent )
+	QFile( parent ),
+	m_bNull( true ),	// The file is supposed to be invaild until its name has been set.
+	m_nSize( -1 )		// The file size is unknown as of now.
 {
-	m_bNull = parent; // Set marker if created with default constructor CFile::CFile().
 }
 
 CFile::CFile(const QString& name, QObject* parent) :
-	m_bNull( false )
+	m_bNull( false ),
+	m_nSize( -1 )		// The file size is unknown as of now.
 {
-	if ( exists() )
-		setTag( "physical" ); // Tag the file as being physically existant on HDD.
+	setup();
 }
 
 bool CFile::removeHash(CHash oHash)
@@ -46,8 +47,31 @@ void CFile::setFileName(const QString& name)
 {
 	m_bNull = false;
 	QFile::setFileName( name );
-	if ( exists() )
-		setTag( "physical" ); // Tag the file as being physically existant on HDD.
+
+	setup();
 }
 
+bool CFile::resize( qint64 sz )
+{
+	bool bSuccess = QFile::resize( sz );
+	if ( bSuccess )
+		setup();
+	return bSuccess;
+}
+
+void CFile::setup()
+{
+	if ( exists() )
+	{
+		setTag( "physical" ); // Tag the file as being physically existant on HDD.
+
+		// TODO: Find out whether it is necessary to open a file to get it size from disk.
+		// size() describes as: "Reimplemented from QIODevice::size()./Returns the size of the file."
+		// This wouldn't be a problem if the description of QIODevice::size() didn't contain:
+		// "If the device is closed, the size returned will not reflect the actual size of the device."
+		open( QIODevice::ReadOnly );
+		m_nSize = QFile::size();
+		close();
+	}
+}
 
