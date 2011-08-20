4b6beae12a7fda185f8ff5ca4e28015db0b3c950
brov <brov@users.sourceforge.net>
2011-08-20 19:58:24 +0000

    Added sent/received fragments counter in datagrams. Fixed datagram sending to prevent net spam when unable to receive datagrams.

--- commonfunctions.cpp
+++ commonfunctions.cpp
@@ -41,7 +41,7 @@ void CommonFunctions::FolderOpen(QString file)
 	{
 		completePath.mkpath( file );
 	}
-	QDesktopServices::openUrl( QUrl( "file:///" + file, QUrl::fromLocalFile ) );
+        QDesktopServices::openUrl( QUrl::fromLocalFile(file) );
 }
 
 QString CommonFunctions::FormatBytes(quint64 nBytesPerSec)
