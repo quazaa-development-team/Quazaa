c3aca3dbf7092f7ec21d735e08f59593ce588a34
Joseph Crowell <joseph.w.crowell@gmail.com>
2011-08-25 19:58:39 +0000

    Added quazaaqglobals.cpp and moved inline functions from quazaaglobals.h to the new file.

--- trunk/Quazaa/quazaaglobals.h
+++ trunk/Quazaa/quazaaglobals.h
@@ -26,62 +26,20 @@
 #define QUAZAAGLOBALS_H
 
 #include <QObject>
-#include "version.h"
 
 class QuazaaGlobals : public QObject
 {
         Q_OBJECT
 public:
-        static QString APPLICATION_NAME()
-	{
-		return "Quazaa";
-	}
-#ifdef QT_DEBUG
-        static QString APPLICATION_VERSION()
-	{
-			return QString().sprintf("%d.%d.r%d (debug build)", Version::MAJOR, Version::MINOR, Version::REVISION);
-	}
-#else
-        static QString APPLICATION_VERSION()
-	{
-		return QString().sprintf("%d.%d.r%d", Version::MAJOR, Version::MINOR, Version::REVISION);
-	}
-#endif
-        static QString APPLICATION_VERSION_STRING()
-	{
-		return QString().sprintf("%d.%d.r%d (%s)", Version::MAJOR, Version::MINOR, Version::REVISION, Version::BUILD_DATE);
-	}
-        static QString APPLICATION_FINGERPRINT()
-	{
-		return "QZ" + APPLICATION_VERSION();
-	}
-        static QString APPLICATION_ORGANIZATION_NAME()
-	{
-		return "Quazaa Development Team";
-	}
-        static QString APPLICATION_ORGANIZATION_DOMAIN()
-	{
-		return "http://quazaa.sourceforge.net";
-	}
-        static QString USER_AGENT_STRING()
-	{
-                return APPLICATION_NAME() + "/" + APPLICATION_VERSION().replace(",", ".");
-	}
-#ifdef QT_DEBUG
-        static QString VENDOR_CODE()
-	{
-		return "QAZB";
-	}
-#else
-        static QString VENDOR_CODE()
-	{
-		return "QAZA";
-	}
-#endif
-        QString MEDIA_OPEN_FILTER()
-	{
-		return tr("All Media Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv *.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma);;" + tr("All Files ") + "(*.*);;" + tr("Video Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv);;" + tr("Audio Files") + " (*.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma)";
-	}
+		static QString APPLICATION_NAME();
+		static QString APPLICATION_VERSION();
+		static QString APPLICATION_VERSION_STRING();
+		static QString APPLICATION_FINGERPRINT();
+		static QString APPLICATION_ORGANIZATION_NAME();
+		static QString APPLICATION_ORGANIZATION_DOMAIN();
+		static QString USER_AGENT_STRING();
+		static QString VENDOR_CODE();
+		QString MEDIA_OPEN_FILTER();
 };
 
 #endif // QUAZAAGLOBALS_H
