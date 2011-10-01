f73d843cef04d25fe65a321017e99ff1c2f6b640
brov <brov@users.sourceforge.net>
2011-10-01 15:50:59 +0000

    Started working on downloads.

---
+++ Quazaa/Transfers/download.h
@@ -0,0 +1,60 @@
+#ifndef DOWNLOAD_H
+#define DOWNLOAD_H
+
+#include "types.h"
+#include "filefragments.h"
+#include "Hashes/hash.h"
+
+class CDownloadSource;
+class CQueryHit;
+
+class CDownload : public QObject
+{
+    Q_OBJECT
+
+public:
+	struct FileListItem
+	{
+		QString sFileName;
+		quint64 nStartOffset;
+		quint64 nEndOffset;
+		QList<CHash> lHashes;
+	};
+	enum DownloadState
+	{
+		dsQueued,
+		dsPaused,
+		dsSearching,
+		dsPending,
+		dsDownloading,
+		dsVerifying,
+		dsMoving,
+		dsFileError,
+		dsCompleted
+	};
+
+	QString					m_sDisplayName;
+	quint64					m_nSize;
+	quint64					m_nCompletedSize;
+	DownloadState			m_nState;
+	QList<CDownloadSource*> m_lSources;
+	bool					m_bMultifile;
+	QList<FileListItem>		m_lFiles;	// for multifile downloads
+	CFileFragments			m_lCompleted;
+	CFileFragments			m_lVerified;
+	CFileFragments			m_lActive;
+	QList<CHash>			m_lHashes; // hashes for whole download
+public:
+	CDownload(CQueryHit* pHit, QObject *parent = 0);
+
+	void start();
+	void pause();
+	void cancelDownload();
+	bool addSource(CDownloadSource* pSource);
+signals:
+
+public slots:
+
+};
+
+#endif // DOWNLOAD_H
---
+++ Quazaa/Transfers/transfers.h
@@ -0,0 +1,10 @@
+#ifndef TRANSFERS_H
+#define TRANSFERS_H
+
+class CTransfers
+{
+public:
+    CTransfers();
+};
+
+#endif // TRANSFERS_H
