e40aaaa6cfa869c3987646d17da31c437a2cf1dd
Joseph Crowell <joseph.w.crowell@gmail.com>
2011-11-26 18:52:18 +0000

    Created new skinsettings class.

---
+++ trunk/Quazaa/Skin/skinsettings.h
@@ -0,0 +1,18 @@
+#ifndef SKINSETTINGS_H
+#define SKINSETTINGS_H
+
+#include <QObject>
+
+class SkinSettings : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SkinSettings(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // SKINSETTINGS_H
