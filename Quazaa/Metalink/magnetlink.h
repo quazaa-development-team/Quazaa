08004d59e874fef0b9f1c641e681e81ed137fd18
old_death <old_death@users.sourceforge.net>
2012-04-26 21:14:33 +0000

    Magnet Link Support: Finished parsing code...

--- Quazaa/Metalink/magnetlink.h
+++ Quazaa/Metalink/magnetlink.h
@@ -11,22 +11,30 @@ class CMagnetLink
 {
 private:
 
+	struct MediaURL
+	{
+		QUrl	m_oURL;
+		quint8	m_nPriority;		// 255: highest priority; 1: lowest priority; 0: temporary disabled
+	};
+
 	// internal storage structure
 	struct File
 	{
-		quint16 m_nID;				// We assume there are no more than 2^16 files in a magnet link.
-		quint64 m_nFileSize;
-		QString m_sFileName;
-		QList<CHash> m_lHashes;		// Includes all hashes provided via <hash> tag.
-		QList<QUrl> m_lURLs;		// Includes http, https, ftp, ftps, etc.
-		QList<QUrl> m_lTrackers;	// BitTorrent Trachers for this file
+		quint16			m_nID;				// We assume there are no more than 2^16 files in a magnet link.
+		quint64			m_nFileSize;
+		QString			m_sFileName;
+		QList<CHash*>	m_lHashes;	// Includes all hashes provided via <hash> tag.
+		QList<MediaURL>	m_lURLs;	// Includes http, https, ftp, ftps, etc.
+		QList<QUrl>		m_lTrackers;	// BitTorrent Trachers for this file
 
 		File();
+		~File();
 		bool isValid();				// Returns true if file struct contains enough data to initialize a download.
 	};
 
-	QString		m_sMagnet;
-	QList<File>	m_lFiles;			// Contains all files sorted by ID.
+	QString			m_sMagnet;
+	QList<File>		m_lFiles;			// Contains all files sorted by ID.
+	QList<QString>	m_lSearches;			// Contains all files sorted by ID.
 
 public:
 	CMagnetLink();
