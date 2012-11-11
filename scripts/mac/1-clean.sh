3090d2dfa7c92072e5576b89504de38a2c62626c
Joseph Crowell <joseph.w.crowell@gmail.com>
2012-11-11 21:49:20 +1000

    Removed site and scripts folders from Qt5 branch.

--- scripts/mac/1-clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-project=`cd "$(dirname $0)/../.."; /bin/pwd`
-svn status --no-ignore "$project/trunk" | egrep '^[?I]' | cut -c9- | xargs rm -rv
--- scripts/mac/2-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-project=`cd "$(dirname $0)/../.."; /bin/pwd`
-
-cd "$project/trunk"
-svn update
