1662739972d92c52ead0a5966c12bf497d2e4495
old_death <old_death@users.sourceforge.net>
2013-01-25 18:34:04 +0000

    [Discovery] Added class for permanently banned services.

---
+++ quazaa/Discovery/banneddiscoveryservice.cpp
@@ -0,0 +1,29 @@
+﻿#include "banneddiscoveryservice.h"
+
+using namespace Discovery;
+
+CBannedDiscoveryService::CBannedDiscoveryService(const QUrl& oURL, const CNetworkType&, quint8 nRating) :
+	CDiscoveryService( oURL, CNetworkType( dpNull ), nRating )
+{
+	m_nServiceType = stBanned;
+}
+
+CBannedDiscoveryService::~CBannedDiscoveryService()
+{
+}
+
+void CBannedDiscoveryService::doQuery() throw()
+{
+	Q_ASSERT( false );
+}
+
+void CBannedDiscoveryService::doUpdate() throw()
+{
+	Q_ASSERT( false );
+}
+
+
+void CBannedDiscoveryService::doCancelRequest() throw()
+{
+	Q_ASSERT( false );
+}
---
+++ quazaa/Discovery/banneddiscoveryservice.h
@@ -0,0 +1,41 @@
+﻿#ifndef BANNEDDISCOVERYSERVICE_H
+#define BANNEDDISCOVERYSERVICE_H
+
+#include "discoveryservice.h"
+
+namespace Discovery
+{
+
+class CBannedDiscoveryService : public CDiscoveryService
+{
+	Q_OBJECT
+
+	/* ================================================================ */
+	/* ========================== Attributes ========================== */
+	/* ================================================================ */
+private:
+	QSharedPointer<QNetworkAccessManager> m_pNAMgr;
+	QNetworkRequest* m_pRequest;
+
+	/* ================================================================ */
+	/* ========================= Construction ========================= */
+	/* ================================================================ */
+public:
+	CBannedDiscoveryService(const QUrl& oURL, const CNetworkType& oNType, quint8 nRating);
+
+	~CBannedDiscoveryService();
+
+	/* ================================================================ */
+	/* ========================== Operations ========================== */
+	/* ================================================================ */
+	inline QString type() const { return QString( "GWC" ); }
+
+private:
+	void doQuery()  throw();
+	void doUpdate() throw();
+	void doCancelRequest() throw();
+};
+
+}
+
+#endif // BANNEDDISCOVERYSERVICE_H
