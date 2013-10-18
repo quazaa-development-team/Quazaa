11bfb47423ccea4c826d71818eb34ff26ccd97f6
Joseph Crowell <joseph.w.crowell@gmail.com>
2013-10-19 06:58:45 +1000

    Darkening the highlight background since the text is now based on the same color.

--- Quazaa/3rdparty/communi-desktop/src/gui/messageview.cpp
+++ Quazaa/3rdparty/communi-desktop/src/gui/messageview.cpp
@@ -515,7 +515,7 @@ void MessageView::applySettings()
 	if (dark) {
 		d.textBrowser->setShadowColor(Qt::black);
 		d.textBrowser->setMarkerColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]));
-		d.textBrowser->setHighlightColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]).darker(265));
+		d.textBrowser->setHighlightColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]).darker(300));
 
 		d.searchEditor->setButtonPixmap(SearchEditor::Left, QPixmap(":/Resource/Chat/Buttons/prev-white.png"));
 		d.searchEditor->setButtonPixmap(SearchEditor::Right, QPixmap(":/Resource/Chat/Buttons/next-white.png"));
@@ -525,7 +525,7 @@ void MessageView::applySettings()
 	} else {
 		d.textBrowser->setShadowColor(Qt::gray);
 		d.textBrowser->setMarkerColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]));
-		d.textBrowser->setHighlightColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]).darker(265));
+		d.textBrowser->setHighlightColor(QColor(quazaaSettings.Chat.Colors[IrcColorType::Highlight]).darker(300));
 
 		d.searchEditor->setButtonPixmap(SearchEditor::Left, QPixmap(":/Resource/Chat/Buttons/prev-black.png"));
 		d.searchEditor->setButtonPixmap(SearchEditor::Right, QPixmap(":/Resource/Chat/Buttons/next-black.png"));
