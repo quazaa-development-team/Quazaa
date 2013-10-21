e656087087b77d5e68699a6cf1829975c9a5937c
Joseph Crowell <joseph.w.crowell@gmail.com>
2013-10-21 20:48:43 +1000

    Removed http:// from APPLICATION_ORGANIZATION_DOMAIN.

--- Quazaa/quazaaglobals.cpp
+++ Quazaa/quazaaglobals.cpp
@@ -29,60 +29,60 @@
 
 QString CQuazaaGlobals::APPLICATION_NAME()
 {
-    return "Quazaa";
+	return "Quazaa";
 }
 
 #ifdef QT_DEBUG
 QString CQuazaaGlobals::APPLICATION_VERSION()
 {
-    return QString().sprintf("%d.%d Git:%s (debug build)", Version::MAJOR, Version::MINOR, Version::REVISION);
+	return QString().sprintf("%d.%d Git:%s (debug build)", Version::MAJOR, Version::MINOR, Version::REVISION);
 }
 #else
 QString CQuazaaGlobals::APPLICATION_VERSION()
 {
-    return QString().sprintf("%d.%d Git:%s", Version::MAJOR, Version::MINOR, Version::REVISION);
+	return QString().sprintf("%d.%d Git:%s", Version::MAJOR, Version::MINOR, Version::REVISION);
 }
 #endif
 
 QString CQuazaaGlobals::APPLICATION_VERSION_STRING()
 {
-    return QString().sprintf("%d.%d Git:%s (%s)", Version::MAJOR, Version::MINOR, Version::REVISION, Version::BUILD_DATE);
+	return QString().sprintf("%d.%d Git:%s (%s)", Version::MAJOR, Version::MINOR, Version::REVISION, Version::BUILD_DATE);
 }
 
 QString CQuazaaGlobals::APPLICATION_FINGERPRINT()
 {
-    return "QZ" + APPLICATION_VERSION();
+	return "QZ" + APPLICATION_VERSION();
 }
 
 QString CQuazaaGlobals::APPLICATION_ORGANIZATION_NAME()
 {
-    return "Quazaa Development Team";
+	return "Quazaa Development Team";
 }
 
 QString CQuazaaGlobals::APPLICATION_ORGANIZATION_DOMAIN()
 {
-    return "http://quazaa.sourceforge.net";
+	return "quazaa.sourceforge.net";
 }
 
 QString CQuazaaGlobals::USER_AGENT_STRING()
 {
-    return APPLICATION_NAME() + "/" + APPLICATION_VERSION().replace(",", ".");
+	return APPLICATION_NAME() + "/" + APPLICATION_VERSION().replace(",", ".");
 }
 
 #ifdef QT_DEBUG
 QString CQuazaaGlobals::VENDOR_CODE()
 {
-    return "QAZB";
+	return "QAZB";
 }
 #else
 QString CQuazaaGlobals::VENDOR_CODE()
 {
-    return "QAZA";
+	return "QAZA";
 }
 #endif
 
 QString CQuazaaGlobals::MEDIA_OPEN_FILTER()
 {
-    return tr("All Media Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv *.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma);;" + tr("All Files ") + "(*.*);;" + tr("Video Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv);;" + tr("Audio Files") + " (*.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma)";
+	return tr("All Media Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv *.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma);;" + tr("All Files ") + "(*.*);;" + tr("Video Files ") + "(*.3g2 *.3gp *.3gp2 *.3gpp *.d2v *.divx *.amr *.amv *.asf *.avi *.bik *.drc *.dsa *.dsm *.dss *.dsv *.evo *.flc *.fli *.flic *.flv *.hdmov *.ifo *.ivf *.m1v *.m2p *.m2t *.m2ts *.m2v *.m4v *.mkv *.mov *.mp2v *.mp4 *.mp4v *.mpe *.mpeg *.mpg *.mpv2 *.mpv4 *.mts *.ogm *.ogv *.pss *.pva *.ram *.ratdvd *.rm *.rmm *.roq *.rp *.rmvb *.rpm *.rt *.smi *.smil *.smk *.swf *.tp *.tpr *.ts *.tta *.vob *.vp6 *.wm *.wmp *.wmv);;" + tr("Audio Files") + " (*.aac *.ac3 *.aif *.aifc *.aiff *.alac *.au *.cda *.dts *.flac *.mid *.midi *.m1a *.m2a *.m4a *.m4b *.mka *.mpa *.mpc *.mp2 *.mp3 *.oga *.ogg *.ra *.rmi *.snd *.wav *.wma)";
 }
 
