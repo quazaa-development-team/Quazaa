695063d2b35eb95ef661b4bda8aac95de3c93d86
Joseph Crowell <joseph.w.crowell@gmail.com>
2013-10-24 20:46:48 +1000

    Don't allow hub to change its IP Address to a local/private IP via LNI is we don't allow Local/Private IPs.

--- Quazaa/NetworkCore/g2node.cpp
+++ Quazaa/NetworkCore/g2node.cpp
@@ -269,7 +269,7 @@ void CG2Node::OnTimer(quint32 tNow)
 		// If hub has been connected for more than 5 minutes and it still has less than 5 leaves,
 		// something is very wrong. Ban it for a short while.
 		if(m_nType == G2_HUB && (tNow - m_tConnected > 300) && m_nLeafCount < 5) {
-			Send_ConnectError("503 Your hub is not accepting leaves.");
+			Send_ConnectError("503 Your hub is not accepting leaves");
 			securityManager.ban(m_oAddress, BanLength::TwoHours, true, QString("Hub not accepting leaves (%1)").arg(m_oAddress.toString()));
 			return;
 		}
@@ -968,14 +968,17 @@ void CG2Node::OnLNI(G2Packet* pPacket)
 
 			if(hostAddr.isValid())
 			{
-				hasNA = true;
+				if(securityManager.isPrivate(hostAddr) && quazaaSettings.Security.IgnorePrivateIP) {
+					Send_ConnectError("422 Local/Private IP addresses are not accepted by this node");
+				} else {
+					hasNA = true;
 
-				if(!m_bInitiated)
-				{
-					m_oAddress = hostAddr;
+					if(!m_bInitiated)
+					{
+						m_oAddress = hostAddr;
+					}
 				}
 			}
-
 		}
 		else if(strcmp("GU", szType) == 0 && nLength >= 16)
 		{
